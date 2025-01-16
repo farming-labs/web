c6e30b8bf14a9ce40c03648c2ffb577f03cecd38
Kinfe123 <kinfishtech@gmail.com>
2025-01-17 01:04:39 +0300

    docs init

--- app/showcase/page.tsx
+++ app/showcase/page.tsx
@@ -6,7 +6,7 @@ export default function ShowCase() {
   return (
     <div className="flex flex-col gap-1">
       <ShoowcaseHeroSection />
-      <Separator className="max-w-[68.3rem] mx-auto" />
+      <Separator className="max-w-[68.3rem] -mt-[1.2px] mx-auto" />
       <Packages />
     </div>
   );
