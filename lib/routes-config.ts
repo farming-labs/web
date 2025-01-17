2a006ea58ab43f7a970a8b1d026860b381a1da4e
Kinfe123 <kinfishtech@gmail.com>
2025-01-17 17:45:28 +0300

    reducing routes on leftbar

--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -14,27 +14,19 @@ export const ROUTES: EachRoute[] = [
     noLink: true,
     items: [
       { title: "Introduction", href: "/introduction" },
-      {
-        title: "Installation",
-        href: "/installation",
-      },
-      { title: "Quick Start Guide", href: "/quick-start-guide" },
-      {
-        title: "Project Structure",
-        href: "/project-structure",
-      },
-      {
-        title: "Components",
-        href: "/components",
-        items: [
-          { title: "Stepper", href: "/stepper" },
-          { title: "Tabs", href: "/tabs" },
-          { title: "Note", href: "/note" },
-          { title: "Code Block", href: "/code-block" },
-          { title: "Image & Link", href: "/image-link" },
-          { title: "Custom", href: "/custom" },
-        ],
-      },
+      { title: "Why FarmingLabs", href: "/why-farming-labs" },
+      // {
+      //   title: "Components",
+      //   href: "/components",
+      //   items: [
+      //     { title: "Stepper", href: "/stepper" },
+      //     { title: "Tabs", href: "/tabs" },
+      //     { title: "Note", href: "/note" },
+      //     { title: "Code Block", href: "/code-block" },
+      //     { title: "Image & Link", href: "/image-link" },
+      //     { title: "Custom", href: "/custom" },
+      //   ],
+      // },
       {
         title: "Befter",
         href: "/befter",
@@ -56,11 +48,6 @@ export const ROUTES: EachRoute[] = [
             ],
           },
         ],
-      },
-      { title: "Themes", href: "/themes" },
-      {
-        title: "Customize",
-        href: "/customize",
       },
     ],
   },
