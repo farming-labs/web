9949737ba1491f4933e8382923ba9b6c8f665446
Kinfe123 <kinfishtech@gmail.com>
2025-01-20 17:01:40 +0300

    padding leaak

--- app/showcase/_components/packages.tsx
+++ app/showcase/_components/packages.tsx
@@ -4,7 +4,7 @@ import { PackagegCard } from "./package-card";
 export default function Packages() {
   return (
     <div className="max-w-6xl p-0 md:p-6 mx-auto overflow-hidden">
-      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 p-10 pt-3 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:p-10 pt-3 lg:grid-cols-3">
         {farmed.map((item, indx) => {
           return <PackagegCard item={item} key={indx} />;
         })}
