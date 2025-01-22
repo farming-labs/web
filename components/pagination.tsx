b6f47724633c05acc36f302767a9bffce84d4b5a
Kinfe123 <kinfishtech@gmail.com>
2025-01-22 04:46:02 +0300

    bordered pagination

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -2,7 +2,18 @@ import { getPreviousNext } from "@/lib/markdown";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
-
+import { cn } from "@/lib/utils";
+const Icon = ({ className, ...rest }: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div
+      {...rest}
+      className={cn(
+        "dark:border-zinc-200/40 border-zinc-700/80 size-6 absolute",
+        className,
+      )}
+    />
+  );
+};
 export default function Pagination({ pathname }: { pathname: string }) {
   const res = getPreviousNext(pathname);
 
@@ -10,38 +21,44 @@ export default function Pagination({ pathname }: { pathname: string }) {
     <div className="grid mb-10 grid-cols-2 flex-grow sm:py-10 py-7 gap-3">
       <div>
         {res.prev && (
-          <Link
-            className={buttonVariants({
-              variant: "outline",
-              className:
-                "no-underline w-full flex flex-col pl-3 !py-8 !items-start",
-            })}
-            href={`/docs${res.prev.href}`}
-          >
-            <span className="flex items-center text-muted-foreground text-xs">
-              <ChevronLeftIcon className="w-[1rem] h-[1rem] mr-1" />
-              Previous
-            </span>
-            <span className="mt-1 ml-1">{res.prev.title}</span>
-          </Link>
+          <div className="px-4 hover:dark:bg-zinc-950 py-3 w-full  flex flex-col  border-2 border-zinc-100 dark:border-zinc-700/20 relative rounded-sm">
+            <Link
+              className="no-underline border-none px-4  w-full hover:bg-transparent flex flex-col !items-start"
+              href={`/docs${res.prev.href}`}
+            >
+              <Icon className="-top-0.5 -left-0.5 border-l-2 border-t-2 rounded-tl-sm" />
+              <Icon className="-top-0.5 -right-0.5 border-r-2 border-t-2 rounded-tr-sm" />
+              <Icon className="-bottom-0.5 -left-0.5 border-l-2 border-b-2 rounded-bl-sm" />
+              <Icon className="-bottom-0.5 -right-0.5 border-r-2 border-b-2 rounded-br-sm" />
+              <span className="flex items-center text-muted-foreground text-xs">
+                <ChevronLeftIcon className="w-[1rem] h-[1rem] mr-1" />
+                Previous
+              </span>
+
+              <span className="mt-1 mr-1">{res.prev.title}</span>
+            </Link>
+          </div>
         )}
       </div>
       <div>
         {res.next && (
-          <Link
-            className={buttonVariants({
-              variant: "outline",
-              className:
-                "no-underline w-full flex flex-col pr-3 !py-8 !items-end",
-            })}
-            href={`/docs${res.next.href}`}
-          >
-            <span className="flex items-center text-muted-foreground text-xs">
-              Next
-              <ChevronRightIcon className="w-[1rem] h-[1rem] ml-1" />
-            </span>
-            <span className="mt-1 mr-1">{res.next.title}</span>
-          </Link>
+          <div className="px-4 hover:dark:bg-zinc-950 py-3 w-full  flex flex-col  border-2 border-zinc-100 dark:border-zinc-700/20 relative rounded-sm">
+            <Link
+              className="no-underline border-none px-4  w-full hover:bg-transparent flex flex-col !items-start"
+              href={`/docs${res.next.href}`}
+            >
+              <Icon className="-top-0.5 -left-0.5 border-l-2 border-t-2 rounded-tl-sm" />
+              <Icon className="-top-0.5 -right-0.5 border-r-2 border-t-2 rounded-tr-sm" />
+              <Icon className="-bottom-0.5 -left-0.5 border-l-2 border-b-2 rounded-bl-sm" />
+              <Icon className="-bottom-0.5 -right-0.5 border-r-2 border-b-2 rounded-br-sm" />
+              <span className="flex items-center text-muted-foreground text-xs">
+                <ChevronRightIcon className="w-[1rem] h-[1rem] mr-1" />
+                Next
+              </span>
+
+              <span className="mt-1 mr-1">{res.next.title}</span>
+            </Link>
+          </div>
         )}
       </div>
     </div>
