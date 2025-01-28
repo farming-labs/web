0adba695724f21936a92b5a2c8c1df9562e67d8e
Kinfe123 <kinfishtech@gmail.com>
2025-01-28 16:20:21 +0300

    smooth edgy

--- components/markdown/pre.tsx
+++ components/markdown/pre.tsx
@@ -6,7 +6,7 @@ const Icon = ({ className, ...rest }: React.HTMLAttributes<HTMLDivElement>) => {
     <div
       {...rest}
       className={cn(
-        "dark:border-zinc-200/20 border-zinc-700/80 size-3 absolute",
+        "dark:border-zinc-200/10 border-zinc-700/80 size-3 absolute",
         className,
       )}
     />
@@ -19,15 +19,17 @@ export default function Pre({
 }: ComponentProps<"pre"> & { raw?: string }) {
   return (
     <div className="my-5  relative">
-      {/* <Icon className="-top-0.5 -left-0.5 border-l-2 border-t-2 rounded-tl-sm" />
-      <Icon className="-top-0.5 -right-0.5 border-r-2 border-t-2 rounded-tr-sm" />
-      <Icon className="-bottom-0.5 -left-0.5 border-l-2 border-b-2 rounded-bl-sm" />
-      <Icon className="-bottom-0.5 -right-0.5 border-r-2 border-b-2 rounded-br-sm" /> */}
+      <Icon className="top-[0.2px] z-20 -left-0.5 border-l-2 border-t-2 rounded-tl-lg" />
+      <Icon className="top-[0.2px] z-20 -right-0.5 border-r-2 border-t-2 rounded-tr-lg" />
+      <Icon className="-bottom-[0.2px] z-20 -left-0.5 border-l-2 border-b-2 rounded-bl-lg" />
+      <Icon className="-bottom-[0.2px] z-20 -right-0.5 border-r-2 border-b-2 rounded-br-lg" />
       <div className="absolute top-3 right-2.5 z-10 sm:block hidden">
         <Copy content={raw!} />
       </div>
-      <div className="relative rounded-none">
-        <pre className="rounded-none">{children}</pre>
+      <div className="relative  rounded-none">
+        <pre className="" {...rest}>
+          {children}
+        </pre>
       </div>
     </div>
   );
