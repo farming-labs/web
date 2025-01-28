cf0b411596c0e6b710b4ba3bedfc24d8502ebb30
Kinfe123 <kinfishtech@gmail.com>
2025-01-28 16:06:35 +0300

    usage docs

--- components/markdown/pre.tsx
+++ components/markdown/pre.tsx
@@ -18,18 +18,16 @@ export default function Pre({
   ...rest
 }: ComponentProps<"pre"> & { raw?: string }) {
   return (
-    <div className="my-5 rounded-none relative">
-      <Icon className="-top-0.5 -left-0.5 border-l-2 border-t-2 rounded-tl-sm" />
+    <div className="my-5  relative">
+      {/* <Icon className="-top-0.5 -left-0.5 border-l-2 border-t-2 rounded-tl-sm" />
       <Icon className="-top-0.5 -right-0.5 border-r-2 border-t-2 rounded-tr-sm" />
       <Icon className="-bottom-0.5 -left-0.5 border-l-2 border-b-2 rounded-bl-sm" />
-      <Icon className="-bottom-0.5 -right-0.5 border-r-2 border-b-2 rounded-br-sm" />
+      <Icon className="-bottom-0.5 -right-0.5 border-r-2 border-b-2 rounded-br-sm" /> */}
       <div className="absolute top-3 right-2.5 z-10 sm:block hidden">
         <Copy content={raw!} />
       </div>
       <div className="relative rounded-none">
-        <pre className="" {...rest}>
-          {children}
-        </pre>
+        <pre className="rounded-none">{children}</pre>
       </div>
     </div>
   );
